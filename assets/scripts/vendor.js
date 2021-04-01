f3fe5503058dfcb8797d616347541299d130cfe5
davzu <davzu_90@hotmail.com>
2021-04-01 02:24:52 -0500

    Add files via upload

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,59 @@
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
+const playerLife = document.getElementById('player-life');
+const shieldNumber = document.getElementById('shield-number');
+
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
+const shieldBtn = document.getElementById('shield-btn');
+
+function adjustHealthBars(maxLife) {
+    monsterHealthBar.max = maxLife;
+    monsterHealthBar.value = maxLife;
+    playerHealthBar.max = maxLife;
+    playerHealthBar.value = maxLife;
+}
+
+function dealMonsterDamage(damage) {
+    const dealtDamage = Math.random() * damage;
+    monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+    return dealtDamage;
+}
+
+function dealPlayerDamage(damage) {
+    const dealtDamage = Math.random() * damage;
+    playerHealthBar.value = +playerHealthBar.value - dealtDamage;
+    return dealtDamage;
+}
+
+function increasePlayerHealth(healValue) {
+    playerHealthBar.value = +playerHealthBar.value + healValue;
+}
+
+function resetGame(value) {
+    playerHealthBar.value = value;
+    monsterHealthBar.value = value;
+}
+
+function removeBonusLife() {
+    bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function addBonusLife() {
+    playerLife.appendChild(bonusLifeEl);
+}
+
+function removeShieldNumber() {
+    shieldNumber.parentNode.removeChild(shieldNumber);
+}
+
+function addShieldNumber() {
+    shieldBtn.appendChild(shieldNumber);
+}
+
+function setPlayerHealth(health) {
+    playerHealthBar.value = health;
+}
